ebc12a24fc40d9216a28f8eaf834ad80adbd6978
Iorverrex <iorvertersur@gmail.com>
2025-03-24 19:52:46 +0100

    my first commit

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const menuToggle = document.getElementById('menu-toggle');
 const menu = document.getElementById('menu');
+const menuLinks = menu.querySelectorAll('a');
 
 menuToggle.addEventListener('click', () => {
   if (menu.style.display === 'flex') {
     menu.style.display = 'none';
   } else {
     menu.style.display = 'flex';
-    menu.style.flexDirection = 'column'; // Ensures vertical alignment
+    menu.style.flexDirection = 'column';
   }
+});
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    menu.style.display = 'none';
+  });
 });
