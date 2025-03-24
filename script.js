aa175df6064c468a4bb177361732fd2133e3cd0b
Iorverrex <iorvertersur@gmail.com>
2025-03-24 20:21:49 +0100

    my first commit

--- script.js
+++ script.js
@@ -3,16 +3,11 @@ const menu = document.getElementById('menu');
 const menuLinks = menu.querySelectorAll('a');
 
 menuToggle.addEventListener('click', () => {
-  if (menu.style.display === 'flex') {
-    menu.style.display = 'none';
-  } else {
-    menu.style.display = 'flex';
-    menu.style.flexDirection = 'column';
-  }
+  menu.classList.toggle('open'); // Toggle class for menu open/close
 });
 
 menuLinks.forEach(link => {
   link.addEventListener('click', () => {
-    menu.style.display = 'none';
+    menu.classList.remove('open'); // Close menu when a link is clicked
   });
 });
